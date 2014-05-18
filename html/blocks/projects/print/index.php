a3c0862fc2062d6cd203aabf7fce817b2fe7be59
zucler <zucler@gmail.com>
2014-05-18 21:22:20 +1000

    Disabled cache for print PDF

--- html/blocks/projects/print/index.php
+++ html/blocks/projects/print/index.php
@@ -1,5 +1,7 @@
 <?php
 ini_set("error_reporting", "(E_ALL ^ E_DEPRECATED)");
+header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 
 require_once(FS_PATH . "php/mpdf/mpdf.php");
 
